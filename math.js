b386f1769f4e24df18211c0de918db668dbb7232
Euzebio <euzebiosantanawork@gmail.com>
2020-07-21 12:13:55 -0300

    version1.0

---
+++ math.js
@@ -0,0 +1,26 @@
+function sum ( a , b ) {
+
+    return a + b ;
+
+} ;
+
+function compoundSum ( a , b , c ) {
+
+    return a + b + c ;
+
+} ;
+
+function multiplication ( a , b ) {
+
+    return a + b ;
+
+} ;
+
+function calc ( a , b , c ) {
+
+    return ( a + b ) * c ;
+
+} ;
+
+module.exports = { sum , compoundSum , multiplication , calc } ;
+
